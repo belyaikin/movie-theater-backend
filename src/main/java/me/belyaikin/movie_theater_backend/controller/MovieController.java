d25575c63468d16373175f5e708f59f8dff67923
belyaikin <dmitriybelyaikin@gmail.com>
2025-11-09 18:38:01 +0500

    Added @CrossOrigin annotation

--- src/main/java/me/belyaikin/movie_theater_backend/controller/MovieController.java
+++ src/main/java/me/belyaikin/movie_theater_backend/controller/MovieController.java
@@ -3,10 +3,7 @@ package me.belyaikin.movie_theater_backend.controller;
 import me.belyaikin.movie_theater_backend.movie.entity.Movie;
 import me.belyaikin.movie_theater_backend.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,11 +13,13 @@ public class MovieController {
     @Autowired
     private MovieService service;
 
+    @CrossOrigin
     @GetMapping("all")
     public List<Movie> getAllMovies() {
         return service.getAllMovies();
     }
 
+    @CrossOrigin
     @GetMapping
     public Movie getMovie(@RequestParam Long id) {
         return service.getMovie(id);
